9b4d86bbe29d1b23f336af26ccf1a9b0c0ce0ced
danieljt84 <danieljt84@gmail.com>
2022-01-11 12:08:27 -0300

    Criado funcoes de add e remove os li

--- src/app/layout-data/form-filter-data/form-filter-data.component.ts
+++ src/app/layout-data/form-filter-data/form-filter-data.component.ts
@@ -16,8 +16,36 @@ export class FormFilterDataComponent implements OnInit {
     $('#industria').val("ola")
   }
 
-  toggleList(){
-			$('#ul').css("visibility","visible");
+  toggleList() {
+    if ($('#ul').css("visibility") == "hidden") {
+      $('#ul').css("visibility", "visible");
+    } else {
+      $('#ul').css("visibility", "hidden");
+    }
+  }
+
+  getValueLi(event: any) {
+    if (!event.target.classList.contains('selected')) {
+      event.target.classList.add('selected');
+      $('#ramo').val(this.addString($('#ramo').val(), event.target.value));
+    } else {
+      event.target.classList.remove('selected');
+      $('#ramo').val(this.removeString($('#ramo').val(), event.target.value));
+    }
+  }
+
+  filterLi(event:any){
+
+  }
+
+  removeString(completeString: string, stringToRemove: string) {
+    completeString = completeString.replace(" " + stringToRemove, "");
+    return completeString;
+  }
+
+  addString(completeString: string, stringToAdd: string): string {
+    completeString = completeString + ' ' + stringToAdd;
+    return completeString;
   }
 
 }
